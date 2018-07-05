47342b1f455f9ff4d06cd34f4377387886d8f318
Matt Gilmore <mtgilmore@comcast.net>
2018-07-05 10:40:39 -0400

    Clean up script

--- web_scraping_for_cats.R
+++ web_scraping_for_cats.R
@@ -1,38 +1,33 @@
 library('rvest')
+library('igraph')
 
-traverse_nodes = function() {
+intitial_page = 'Cat'
+initial_link = paste('https://en.wikipedia.org/wiki/', intitial_page)
+webpage = read_html(initial_link)
 
-  intitial_page = 'Cat'
-  initial_link = paste('https://en.wikipedia.org/wiki/', intitial_page)
-  webpage = read_html(initial_link)
+text_data_html = html_nodes(webpage, '.mw-parser-output p')
+child = html_children(text_data_html)
+child = as.character(child)
+
+links = array('')
+count = 1
+
+# search all tags from the page
+for (i in 1:length(child)) {
   
-  text_data_html = html_nodes(webpage, '.mw-parser-output p')
-  #text_data = html_text(text_data_html)
-  #name = html_name(text_data_html)
-  #attrs = html_attrs(text_data_html)
-  child = html_children(text_data_html)
-  child = as.character(child)
-  
-  links = array('')
-  count = 1
-  
-  # search all tags from the page
-  for (i in 1:length(child)) {
+  # look for a wiki link
+  if (substr(child[i],1,15) == "<a href=\"/wiki/") {
     
-    # look for a wiki link
-    if (substr(child[i],1,15) == "<a href=\"/wiki/") {
-      
-      # update list of all links
-      append(links, "")
-      
-      # extract name of each page
-      j = 16
-      while (substr(child[i],j,j) != "\"") {
-        links[count] = substr(child[i],16,j)
-        j = j + 1
-      }
-      count = count + 1
+    # update list of all links
+    append(links, "")
+    
+    # extract name of each page
+    j = 16
+    while (substr(child[i],j,j) != "\"") {
+      links[count] = substr(child[i],16,j)
+      j = j + 1
     }
-    
+    count = count + 1
   }
 }
+
